b29fd119aa698b0ee38429d36be8dc7508c35a28
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-21 22:24:23 +0000

    wrong group

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ LOGFILE=/var/log/supervisor/twobbler/gunicorn.log
 NUM_WORKERS=3
 ADDRESS=127.0.0.1:3000
 USER=mat
-GROUP=mat
+GROUP=staff
 
 cd /Users/$USER/src/chandelier_infohub/
 source /Users/$USER/.virtualenvs/chandelier_infohub/bin/activate
