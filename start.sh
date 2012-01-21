2f48b2922fc8ed9b31b3d63fe67785b92c4b716a
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-21 22:20:44 +0000

    gunicorn deployment

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ ADDRESS=127.0.0.1:3000
 USER=mat
 GROUP=mat
 
-cd /home/$USER/src/chandelier_infohub/
-source /home/$USER/.virtualenvs/chandelier_infohub/bin/activate
-exec /home/$USER/.virtualenvs/chandelier_infohub/bin/gunicorn_django -w $NUM_WORKERS \
+cd /Users/$USER/src/chandelier_infohub/
+source /Users/$USER/.virtualenvs/chandelier_infohub/bin/activate
+exec /Users/$USER/.virtualenvs/chandelier_infohub/bin/gunicorn_django -w $NUM_WORKERS \
     --user=$USER --group=$GROUP --bind $ADDRESS
