23ee89f65f538d8703953538a910b90a20353094
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-23 23:28:52 +0000

    styled the web

--- doorbell/views.py
+++ doorbell/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from utils.quatz import start_doorbell_mode
+from utils.quatz import doorbell_mode
 
 def activate(request):
-    start_doorbell_mode()
+    doorbell_mode()
     return HttpResponse('')
