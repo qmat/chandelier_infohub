adc50ce2185361705231a666d76c73da2bf11114
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-23 23:08:40 +0000

    styled the web

--- doorbell/views.py
+++ doorbell/views.py
@@ -3,5 +3,5 @@ from utils.quatz import web_mode
 
 def activate(request):
     #web_mode(1, ['http://127.0.0.1:8000/static/doorbell_stream.html'])
-    web_mode(8, ['http://10.0.2.1/~MAT/ipcamimages/lastimage.php'])
+    web_mode(8, ['http://10.0.2.1/~MAT/ipcamimages/index.php'])
     return HttpResponse('')
