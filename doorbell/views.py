3b3f673cb854b13ecb69cb5ace8274420ffddbe6
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-23 21:58:07 +0000

    added doorbell changes

--- doorbell/views.py
+++ doorbell/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from utils.quatz import web_mode
 
-def new_preset(request):
+def activate(request):
     web_mode(8, ['http://127.0.0.1:8000/static/doorbell_stream.html'])
     return HttpResponse('')
