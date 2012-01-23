e2251512988b7a2f9bc6a1b742fda88244ae534c
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-23 22:03:54 +0000

    adding pw

--- doorbell/views.py
+++ doorbell/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from utils.quatz import web_mode
 
 def activate(request):
-    web_mode(8, ['http://admin:@127.0.0.1:8000/static/doorbell_stream.html'])
+    web_mode(8, ['http://127.0.0.1:8000/static/doorbell_stream.html'])
     return HttpResponse('')
