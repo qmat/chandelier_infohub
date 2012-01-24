ea60cd70ab7c2f409e41eaa8c0c694d10068e1bc
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-01-24 00:14:03 +0000

    using cache deliberately

--- utils/quatz.py
+++ utils/quatz.py
@@ -6,7 +6,7 @@ quatz_instance = False
 
 def doorbell_mode():
     web_mode(8, ['http://10.0.2.1/~MAT/ipcamimages/index.html'])
-    subprocess.call('play /Users/mat/src/chandelier_infohub/static/69385__guitarguy1985__doorbell2.wav', shell=True)
+    subprocess.call('/usr/local/bin/play /Users/mat/src/chandelier_infohub/static/69385__guitarguy1985__doorbell2.wav', shell=True)
     time.sleep(15)
     web_mode(4, ['http://127.0.0.1:8000/updates/qnn/'])
 
